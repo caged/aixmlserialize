564bae2cb5cf121e54b8fef2ff4f6d07d0f02ab3
Justin Palmer <justin@activereload.net>
2009-02-24 17:33:38 -0800

    crap

--- NSXMLElement+Serialize.m
+++ NSXMLElement+Serialize.m
@@ -35,7 +35,19 @@
     // Create distinct arrays for items with the same name
     //NSArray *keys = [self valueForKeyPath:@"children.@distinctUnionOfObjects.name"];
     NSString *type = [[self attributesAsDictionary] valueForKey:@"type"];
+
     
+    //File
+    if([type isEqualToString:@"file"])
+    {
+        return [NSDictionary dictionary];
+    } 
+    
+    if([self kind] == NSXMLTextKind)
+    {
+        NSLog(@"%s =====================================IS TEXT KIND", _cmd);
+    }
+        
     for(node in nodes) 
     {
         NSMutableArray *group;
@@ -44,15 +56,25 @@
         if(!group)
         {
             group = [NSMutableArray array];
+            if([node kind] == NSXMLTextKind)
+            {
+                NSLog(@"TEXT NODE:%@", node);
+                
+            }
+            NSLog(@"--------------------------------------------%@", node);
+            if(elementName)
             [groups setObject:group forKey:elementName];
-        }
+        } 
         
         [group addObject:node];
     }
     
-    // Array
+    NSLog(@"%s GROUPS:%@", _cmd, groups);
+    
+    //Array
     if([type isEqualToString:@"array"])
     {
+        NSLog(@"%s ITS AN ARRAy", _cmd);
         out = [NSMutableArray array];
         NSString *key;
         for(key in groups)
@@ -77,43 +99,49 @@
     } 
     else 
     {
+        NSLog(@"%s ITS A HASH", _cmd);
         out = [NSMutableDictionary dictionary];
         NSString *key;
         for(key in groups)
-        {
-
-                                    
-            id obj = [groups objectForKey:key];
+        {                           
+            NSArray *obj = [groups objectForKey:key];
+            //NSLog(@"%s obj:%i", _cmd, [obj count]);
+            // There is only one object of this kind
             if([obj count] == 1)
             {                
-                if([key isEqualToString:@"committee"])
-                    NSLog(@"KEY:%@ OBJ:%@", key, [obj objectAtIndex:0]);
-                       
-                id finalObj = [obj objectAtIndex:0]; 
-                if([[finalObj children] count] > 0)
-                {
-                    id childObj = [finalObj childAtIndex:0];
-                    if([childObj kind] == NSXMLTextKind)
-                    {       
-                        NSString *contents = [childObj stringValue];
-                        [out setObject:contents forKey:key];
-                    } 
-                    else 
-                      {         
-                          NSMutableDictionary *subObj = [[finalObj toDictionary] valueForKey:key];
-                          [out setObject:subObj forKey:[finalObj name]];
-                      }
-
-                } 
-                else
-                { 
-                    NSString *contents = [finalObj stringValue];
-                    NSString *objName = [finalObj name];
-                    [out setObject:contents forKey:objName];
-                }
+                //NSLog(@"HASH IS EQUAL TO ONE");
+                id subObj = [[obj objectAtIndex:0] toDictionary];
+                if(subObj == nil)
+                NSLog(@"-------------------------------------------------------SUB OBJ:%@", subObj);
+                //[out addEntriesFromDictionary:]
+                // if([key isEqualToString:@"committee"])
+                //     NSLog(@"KEY:%@ OBJ:%@", key, [obj objectAtIndex:0]);
+                //        
+                // id finalObj = [obj objectAtIndex:0]; 
+                // if([[finalObj children] count] > 0)
+                // {
+                //     id childObj = [finalObj childAtIndex:0];
+                //     if([childObj kind] == NSXMLTextKind)
+                //     {       
+                //         NSString *contents = [childObj stringValue];
+                //         [out setObject:contents forKey:key];
+                //     } 
+                //     else 
+                //       {         
+                //           NSMutableDictionary *subObj = [[finalObj toDictionary] valueForKey:key];
+                //           [out setObject:subObj forKey:[finalObj name]];
+                //       }
+                // 
+                // } 
+                // else
+                // { 
+                //     NSString *contents = [finalObj stringValue];
+                //     NSString *objName = [finalObj name];
+                //     [out setObject:contents forKey:objName];
+                // }
             } 
             else
-            {        
+            {   
                 NSMutableArray *subOut = [NSMutableArray array];
                 for(id el in obj)
                 {       
